5cca76c81f9b42a2c34f0dcb9361190fa731565b
gargmanav <manavgarg1998@gmail.com>
2023-02-18 00:49:15 +0530

    third assignment

---
+++ React-jsx-002/firstproject/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <h1>The Evolution of Airbnb's Frontend</h1>
+    <h3>@spikebrehm</h3>
+    </>
+  );
+}
+
+export default App;
